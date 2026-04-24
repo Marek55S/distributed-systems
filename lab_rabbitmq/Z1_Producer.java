3a9df9dfaff8165ac9cc6e79a96388a6fe34207b
marekswakon <38371062+Marek55S@users.noreply.github.com>
2026-04-24 23:16:25 +0200

    Lab4 (#7)
    * main and html templates in hw2
    
    * code works, but need some changes to get max score
    
    * code looks better, html and data analysis needs final touch
    
    * html updated, need to add more medical analysis
    
    * final version of homework (i hope)
    
    * code after lab 3
    
    * point 13 finished
    
    * point 23 finished
    
    * lab3 finished
    
    * kod po lab3
    
    * code after some labs

---
+++ lab_rabbitmq/Z1_Producer.java
@@ -0,0 +1,47 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Z1_Producer {
+
+    public static void main(String[] argv) throws Exception {
+
+        // info
+        System.out.println("Z1 PRODUCER");
+
+        // connection & channel
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        // queue
+        String QUEUE_NAME = "queue1";
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // producer (publish msg)
+//        String message = br.readLine();
+
+
+        for (int i = 0; i < 9; i++) {
+            String message;
+            if(i%2==0){ message = "1";
+            }
+            else{
+                 message = "5";
+            }
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println("Sent: " + message);
+        }
+
+
+        // close
+        channel.close();
+        connection.close();
+    }
+}
